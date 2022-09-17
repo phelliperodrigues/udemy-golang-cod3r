10354072bdb9b764013645a41b922793332f22ac
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-09-17 16:08:35 -0300

    :sparkles: Operadores Lógicos

---
+++ 3 - Fundamentos/operadores/logicos/logicos.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func compras(trabalho1, trabalho2 bool) (bool, bool, bool) {
+	comprarTv50 := trabalho1 && trabalho2
+	comprarTv32 := trabalho1 != trabalho2 // ou exclusivo
+	comprarSorvete := trabalho1 || trabalho2
+
+	return comprarTv50, comprarTv32, comprarSorvete
+
+}
+
+func main() {
+
+	tv50, tv32, sorvete := compras(true, true)
+	fmt.Printf("Tv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(false, true)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(true, false)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+
+	tv50, tv32, sorvete = compras(false, false)
+	fmt.Printf("\nTv50: %t, Tv32: %t, Sorvete: %t, Saudável: %t", tv50, tv32, sorvete, !sorvete)
+
+}
