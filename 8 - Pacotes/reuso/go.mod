27ff8e4094a22f62c0f52133498513a167f9b641
Phellipe Rodrigues <phellipe.rodrigues@mercadolivre.com>
2022-10-24 15:52:23 -0300

    :sparkles: Criando um pacote reutilizável / Instalando pacote a partir do github

---
+++ 8 - Pacotes/reuso/go.mod
@@ -0,0 +1,5 @@
+module reuso
+
+go 1.19
+
+require github.com/phelliperodrigues/udemy_golang_cod3r_area v0.0.0-20221024184219-f261d5599392
---
+++ 8 - Pacotes/reuso/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	area "github.com/phelliperodrigues/udemy_golang_cod3r_area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(5.0, 2.0))
+}
