699faca3a007cd6d1f1938997a41ed112647d0ba
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-10-26 22:24:14 -0300

    :sparkles: Gerando relatório de cobertura de teste

--- 10 - Testes/matematica/matemastica.go
+++ 10 - Testes/matematica/matemastica.go
@@ -10,7 +10,25 @@ func Media(numeros ...float64) float64 {
 	for _, num := range numeros {
 		total += num
 	}
+
+	if total < 0 {
+		fmt.Println("Sem cobertura de teste")
+	}
 	media := total / float64(len(numeros))
 	mediaArrendondada, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", media), 64)
 	return mediaArrendondada
 }
+
+/*
+Roda teste e exibe cobertura
+go test -cover
+
+Roda teste e gera relatorio
+go test cover -func=resultado.out
+
+Exibe resultado relatorio no terminal
+go test -coverprofile=resultado.out
+
+Exibe resultado relatorio em uma pagina html
+go tool cover -html=resultado.out
+*/
